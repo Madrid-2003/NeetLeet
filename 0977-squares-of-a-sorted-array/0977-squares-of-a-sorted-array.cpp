4603a167316f350b46328271cc6d2648feb18ae0
Madrid-2003 <www.madridmandal.in@gmail.com>
2026-01-08 23:38:27 +0530

    Time: 11 ms (17.26%), Space: 31.2 MB (17.87%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+    vector<int>ans;
+for(int i=0;i<nums.size();i++){
+    int var=nums[i]*nums[i];
+    ans.push_back(var);
+    
+}
+sort(ans.begin(),ans.end());
+return ans;
+    }
+};
