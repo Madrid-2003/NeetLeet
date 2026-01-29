aec3405b66b096d9d879a49ac377f808d1bcb386
Madrid-2003 <www.madridmandal.in@gmail.com>
2026-01-29 10:11:20 +0530

    Time: 59 ms (23.88%), Space: 29.2 MB (45.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        int n = nums.size();
-        
-        sort(nums.begin(), nums.end());
+vector<vector<int>>ans;
+int n=nums.size();
+sort(nums.begin(),nums.end());
+for(int i=0;i<nums.size();i++){
+    int left=i+1;
+    int right=nums.size()-1;
+    if(i>0 && nums[i]==nums[i-1]){
+        continue;
+    }
+    while(left<right){
+            int sum=nums[i]+nums[left]+nums[right];
+if(sum==0){
+ ans.push_back({nums[i],nums[left],nums[right]});
 
-        for (int i = 0; i < n; i++) {
-            // Skip duplicate fixed elements
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+    while(left<right && nums[left]==nums[left+1]){
+        left++;
+    }
+    while(left<right&& nums[right]==nums[right-1])
+    {
+        right--;
+    }
+    left++;
+    right--;
+}
 
-            int l = i + 1, r = n - 1;
+else if(sum>0){
+    right--;
+}
+else{left++;
+}
 
-            while (l < r) {
-                long long sum = (long long)nums[i] + nums[l] + nums[r];
+    }
+}
+return ans;
 
-                if (sum == 0) {
-                    res.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
-
-                    // Skip duplicate left & right values
-                    while (l < r && nums[l] == nums[l - 1]) l++;
-                    while (l < r && nums[r] == nums[r + 1]) r--;
-                }
-                else if (sum < 0) {
-                    l++;
-                }
-                else {
-                    r--;
-                }
-            }
-        }
-        return res;
     }
 };
