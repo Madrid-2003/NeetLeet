76378d938fd49949c0051bd1e15c27a09b799b18
Madrid-2003 <www.madridmandal.in@gmail.com>
2026-02-28 15:47:07 +0530

    Time: 18 ms (28.68%), Space: 36.8 MB (34.85%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+//         int n = arr.size();
+//         sort(begin(arr), end(arr));
+//         int minD= INT_MAX;
+//         for(int i=1; i<n;++i){
+//             minD= min(minD, arr[i]- arr[i-1]);
+
+//         }
+//         vector<vector<int>> result;
+//         for(int i=1; i<n;++i){
+//             int diff = arr[i]-
+//             ?"  arr[i-1];
+
+//             if(diff==minD){
+//                 result.push_back({arr[i-1], arr[i]});
+
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        int n = arr.size();
-        sort(begin(arr), end(arr));
-        int minD= INT_MAX;
-        for(int i=1; i<n;++i){
-            minD= min(minD, arr[i]- arr[i-1]);
+     int n=arr.size();
+     sort(arr.begin(),arr.end());
+     vector<vector<int>>result;
+     int mindiff=INT_MAX;
+     for(int i=1;i<arr.size();i++){
+        mindiff=min(mindiff,arr[i]-arr[i-1]);
 
+        
+     }  
+     for(int i=1;i<arr.size();i++){
+        if(arr[i]-arr[i-1]==mindiff){
+            result.push_back({arr[i-1],arr[i]});
         }
-        vector<vector<int>> result;
-        for(int i=1; i<n;++i){
-            int diff = arr[i]-arr[i-1];
-
-            if(diff==minD){
-                result.push_back({arr[i-1], arr[i]});
-
-            }
-        }
-        return result;
+     } 
+     return result;
     }
 };
