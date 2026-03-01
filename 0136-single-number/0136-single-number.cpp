ab2994c803a07550384e9dd8f9f306cf5bad6ef3
Madrid-2003 <www.madridmandal.in@gmail.com>
2026-03-01 11:56:16 +0530

    Time: 0 ms (100%), Space: 20.6 MB (82.8%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,30 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         unordered_map<int,int>freq;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             freq[nums[i]]++;
+//         }
+//         for(auto &p:freq){
+//             if (p.second==1){
+//                 return p.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            freq[nums[i]]++;
-        }
-        for(auto &p:freq){
-            if (p.second==1){
-                return p.first;
-            }
-        }
-        return -1;
+      int ans=0;
+      int n=nums.size();
+      for(int i=0;i<n;i++){
+        ans=ans^nums[i];
+      }
+      return ans;
     }
 };
